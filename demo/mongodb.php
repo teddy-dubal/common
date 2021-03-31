fedb6379e69ee910b8b0c5fa63addf455cd00120
Teddy Dona <teddy.dubal@gmail.com>
2021-03-31 12:34:56 -0400

    * add datetime type for mongodb

--- demo/mongodb.php
+++ demo/mongodb.php
@@ -1,112 +1,83 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Common\Factory\ModelFactory;
+use App\Common\Factory\DocumentFactory;
+use App\Common\ServiceProvider\MongoDb;
 use App\Model\Entity\User;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Pimple\Container;
-use Zend\Db\Adapter\Adapter;
 
+$config = require_once __DIR__ . '/config/config.php';
+$app    = new Container();
 
-
-$config    = require_once __DIR__ . '/config/config.php';
-$app       = new Container();
-$app['db'] = array(
-    'driver'   => 'Pdo_Mysql',
-    'host'     => $config['db.host'],
-    'dbname'   => 'common',
-    'username' => $config['db.user'],
-    'password' => $config['db.password'],
-    'charset'  => 'utf8',
-);
-$app['logger'] = function ($container) {
-    $log = new Logger('App-Common-Log');
+$app['mongodb_conf'] = $config['mongodb'];
+$app['logger']       = function ($container) {
+    $log = new Logger('App-Common-Mongo-Log');
     $log->pushHandler(new StreamHandler('php://stderr'));
     return $log;
 };
-$app['entity'] = function() use ($app) {
-    $db           = new Adapter($app['db']);
-    $modelFactory = new ModelFactory($db, $app);
-    return $modelFactory;
+$app['document'] = function () use ($app) {
+    return new DocumentFactory($app, $app['mongodb_conf']['mongodb.options']['database']);
 };
+$app->register(new MongoDb(), $app['mongodb_conf']);
 
 $timestart   = microtime(true);
-$userManager = $app['entity']->get('\App\Model\Table\User');
+$userManager = $app['document']->get('\App\Model\Document\User');
 $nbUser      = 10;
 echo "***********************************************" . PHP_EOL;
 //Ajouter x users
 echo sprintf('Ajout de %s users', $nbUser) . PHP_EOL;
 for ($i = 0; $i < $nbUser; $i++) {
-    $user   = new User();
-    $user->setOptions(array('name' => 'Teddy_' . $i));
-    $user->exchangeArray(array('name' => 'Teddy'));
-    $result = $userManager->saveEntity($user);
+    $user = new User();
+    $user->setOptions(array('name' => 'Teddy_' . $i, 'created_at' => 'now'));
+    // $user->exchangeArray(array('name' => 'Teddy'));
+    $result = $userManager->saveDocument($user);
     echo sprintf('User id:%s', $result) . PHP_EOL;
 }
 echo "***********************************************" . PHP_EOL;
 //Afficher les x users
 echo sprintf('Affichage des %s users', $nbUser) . PHP_EOL;
-$result   = $userManager->findBy([]);
+$result   = $userManager->findDocBy([]);
 $id_Users = array();
 foreach ($result as $v) {
     var_dump($v);
-    $id_Users[] = $v['id'];
+    $id_Users[] = current($v['_id']);
 }
-$key = array_rand($id_Users);
-$id  = $id_Users[$key];
-echo "***********************************************" . PHP_EOL;
-echo sprintf('Find user id : %s', $id) . PHP_EOL;
-$u   = $userManager->find($id);
-
-var_dump($u);
-
 echo "***********************************************" . PHP_EOL;
 $name = 'Teddy';
 echo sprintf('Find user by criteria (name) : %s', $name) . PHP_EOL;
-$u    = $userManager->findBy(array('name' => $name), 'id ASC', 10, null, false);
+$u = $userManager->findDocBy(array('name' => $name));
 
 var_dump($u);
 echo "***********************************************" . PHP_EOL;
 
 echo sprintf('Find user by criteria (name) in debug mode : %s', $name) . PHP_EOL;
 $userManager->setDebug(true);
-$u    = $userManager->findBy(array('name' => $name), 'id ASC', 10, null, false);
+$u = $userManager->findDocBy(array('name' => $name));
 var_dump($u);
 $userManager->setDebug(false);
 echo "***********************************************" . PHP_EOL;
 $name = 'Teddy';
 echo sprintf('Count by criteria (name) : %s', $name) . PHP_EOL;
-$u    = $userManager->countBy(array('name' => $name));
+$u = $userManager->count(array('name' => $name));
 
 var_dump($u);
 
 echo "***********************************************" . PHP_EOL;
 $name = 'Teddy';
 echo sprintf('FindOneBy With result : %s', $name) . PHP_EOL;
-$u    = $userManager->findOneBy(array('name' => $name));
+$u = $userManager->findOneDocBy(array('name' => $name));
 var_dump($u);
 echo "***********************************************" . PHP_EOL;
 $name = 'Meddy';
 echo sprintf('FindOneBy Without result : %s', $name) . PHP_EOL;
-$u    = $userManager->findOneBy(array('name' => $name));
-var_dump($u);
-echo "***********************************************" . PHP_EOL;
-$name = 'Teddy';
-echo sprintf('FindOneEntityBy With result : %s', $name) . PHP_EOL;
-$u    = $userManager->findOneEntityBy(array('name' => $name));
-var_dump($u);
-echo "***********************************************" . PHP_EOL;
-$name = 'Meddy';
-echo sprintf('FindOneEntityBy Without result : %s', $name) . PHP_EOL;
-$u    = $userManager->findOneEntityBy(array('name' => $name));
+$u = $userManager->findOneDocBy(array('name' => $name));
 var_dump($u);
 echo "***********************************************" . PHP_EOL;
 echo sprintf('Suppression des %s users', $nbUser) . PHP_EOL;
 foreach ($id_Users as $v) {
-    $userToDelete = new User();
-    $userToDelete->setId($v);
-    $res          = $userManager->deleteEntity($userToDelete);
+    // $res = $userManager->deleteOne(['_id' => new \MongoDB\BSON\ObjectId($v)]);
 }
 
 echo "***********************************************" . PHP_EOL;
---
+++ tests/src/Model/Document/AccountsTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace  Tests\App\Model\Document;
+
+use App\Model\Entity\Accounts as AccountsDocument;
+use PHPUnit\Framework\TestCase;
+
+final class AccountsTest extends TestCase {
+}
+
